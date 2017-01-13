1320e67087e6cb2f30a656b9c5095e6c9198e0b4
Carlospinedat <carlospinedat@gmail.com>
2017-01-13 09:05:08 -0500

    Nuevos Themes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 
 $(function () {
+    
+    alert('mmm');
+    
     $("span[data-theme]").click(function () {
         $("head link#theme").attr("href", 'css/color/' + $(this).data("theme") + '.css');
     });
