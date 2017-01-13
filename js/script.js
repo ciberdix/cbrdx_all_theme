0ea8ccbe8ef4450f0bfb1401befa1462cfcc5a72
Carlospinedat <carlospinedat@gmail.com>
2017-01-13 09:53:09 -0500

    Variable theme in Storage

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
+/**
+ * 
+ */
+ if(sessionStorage.datatheme === undefined){
+     $("head link#theme").attr("href", 'css/color/' + 'white' + '.css');
+ }else{
+     $("head link#theme").attr("href", 'css/color/' + sessionStorage.datatheme + '.css');
+ }
+
+
 
 $(function () {
-    
-    alert('mmm');
-    
+
     $("span[data-theme]").click(function () {
+        theme = $(this).data("theme");
         $("head link#theme").attr("href", 'css/color/' + $(this).data("theme") + '.css');
+        sessionStorage.setItem("datatheme", theme );
     });
 });
 
